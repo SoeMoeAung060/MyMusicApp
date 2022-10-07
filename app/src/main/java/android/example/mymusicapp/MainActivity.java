6ddef8db6afcc6871b2865c8efa1c4b4c54aeb5d
Soe Moe Aung <ahshan061@gmail.com>
2022-10-07 20:44:22 +0800

    My Music App

--- app/src/main/java/android/example/mymusicapp/MainActivity.java
+++ app/src/main/java/android/example/mymusicapp/MainActivity.java
@@ -2,20 +2,88 @@ package android.example.mymusicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+
+        // Find the View that shows the relax_button_view
+        Button relaxButton = findViewById(R.id.relax_button_view);
+        // Set a click listener on that View
+        relaxButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent relaxIntent = new Intent(MainActivity.this, RelaxActivity.class);
+                // Start the new activity
+                startActivity(relaxIntent);
+
+            }
+        });
+
+        Button workoutButton = findViewById(R.id.workout_button_view);
+        workoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent workoutIntent = new Intent(MainActivity.this, WorkoutActivity.class);
+                startActivity(workoutIntent);
+
+            }
+        });
+
+        Button energizeButton = findViewById(R.id.energize_button_view);
+        energizeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent EnergizeIntent = new Intent(MainActivity.this, EnergizeActivity.class);
+                startActivity(EnergizeIntent);
+
+            }
+        });
+
+        Button commuteButton = findViewById(R.id.commute_button_view);
+        commuteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent commuteIntent = new Intent(MainActivity.this, CommuteActivity.class);
+                startActivity(commuteIntent);
+
+            }
+        });
+
+        Button focusButton = findViewById(R.id.focus_button_view);
+        focusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent focusIntent = new Intent(MainActivity.this, FocusActivity.class);
+                startActivity(focusIntent);
+
+            }
+        });
+
+
+        TextView musicPlayPath = findViewById(R.id.music_play_path);
+        musicPlayPath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent musicPlayPathActivity = new Intent(MainActivity.this, MusicActivity.class);
+                startActivity(musicPlayPathActivity);
+            }
+        });
 
 
     }
--- app/src/main/java/android/example/mymusicapp/MusicActivity.java
+++ app/src/main/java/android/example/mymusicapp/MusicActivity.java
@@ -3,12 +3,39 @@ package android.example.mymusicapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MusicActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_music);
+        setContentView(R.layout.music_list);
+
+
+        ArrayList<Music> musicArrayList = new ArrayList<>();
+
+        musicArrayList.add(new Music("Perfect", "Ed Sheeran",R.drawable.profile_1));
+        musicArrayList.add(new Music("Girls Like You", "Maroon5",R.drawable.profile_2));
+        musicArrayList.add(new Music("Something Just Like This", "The Chainsmokers",R.drawable.profile_3));
+        musicArrayList.add(new Music("yeah!", "usher",R.drawable.profile_4));
+        musicArrayList.add(new Music("All of Me", "John Legend",R.drawable.profile_5));
+        musicArrayList.add(new Music("Right here Waiting", "Music Travel Love",R.drawable.profile_6));
+        musicArrayList.add(new Music("Where is the Love", "The Black Eyed Peas",R.drawable.profile_7));
+        musicArrayList.add(new Music("Love Yourself", "JustinBieber",R.drawable.album6));
+        musicArrayList.add(new Music("Dangerous (feat.Akon)", "Kardubak Offishall",R.drawable.album5));
+        musicArrayList.add(new Music("One Day", "Matisyahu",R.drawable.album4));
+        musicArrayList.add(new Music("Marry You", "Bruno Mars",R.drawable.album3));
+        musicArrayList.add(new Music("One Call Away", "Charlie Puth",R.drawable.album2));
+        musicArrayList.add(new Music("Night Changes", "One Direction",R.drawable.album1));
+
+
+        MusicAdapter musicAdapter = new MusicAdapter(this, musicArrayList,R.color.black_status_bar);
+
+        ListView relaxListView = (ListView) findViewById(R.id.list);
+        relaxListView.setAdapter(musicAdapter);
+
     }
 }
